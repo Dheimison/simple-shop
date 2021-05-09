a1a09fca28a42df4bbd23e1648244c894b2312ed
Dheimison Siqueira <dheimisonlo@gmail.com>
2021-05-09 16:45:24 -0300

    feat: imports Routes and initializes icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { getTheme } from '@fluentui/react';
+import { getTheme, initializeIcons } from '@fluentui/react';
 import { ThemeProvider } from 'styled-components';
 
+import { Routes } from 'routes';
 import GlobalStyle from 'styles/global';
 
 function App() {
+  initializeIcons();
   const theme = getTheme();
 
   return (
     <ThemeProvider theme={theme}>
+      <Routes />
       <GlobalStyle />
     </ThemeProvider>
   );
