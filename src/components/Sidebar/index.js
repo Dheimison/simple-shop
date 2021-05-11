fe15c9770fbd32d9b3d7e6d8486b43d6b7ef84a6
Dheimison Siqueira <dheimisonlo@gmail.com>
2021-05-10 22:23:11 -0300

    refactor: improves Sidebar component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,44 +1,39 @@
-import { Nav } from '@fluentui/react';
-import { useLocation } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { useContext, useEffect, useState } from 'react';
 import { MainContext } from 'contexts';
+import { Navbar } from 'components/Navbar';
 import * as S from './styles';
 
-export function Sidebar({ onNavClick, links }) {
-  const [navLinks, setNavLinks] = useState(null);
+function formatNavLinks(links, count) {
+  return links.map((link) => ({
+    name: `${link.name} ${
+      count[link.resource] ? `(${count[link.resource]})` : ''
+    }`,
+    icon: link.iconName,
+    key: link.path,
+  }));
+}
+
+export function Sidebar({ links }) {
+  const [navLinks, setNavLinks] = useState([]);
   const { count } = useContext(MainContext);
-  const location = useLocation();
 
   useEffect(() => {
     if (links) {
-      const formattedLinks = links.map((link) => ({
-        name: `${link.name} ${
-          count[link.resource] ? `(${count[link.resource]})` : ''
-        }`,
-        icon: link.iconName,
-        key: link.path,
-      }));
-
+      const formattedLinks = formatNavLinks(links, count);
       setNavLinks([{ links: formattedLinks }]);
     }
   }, [links, count]);
 
   return (
     <S.Container>
-      <Nav
-        selectedKey={location.pathname}
-        ariaLabel="Sidebar navigation"
-        groups={navLinks}
-        onLinkClick={(_, item) => item && item.key && onNavClick(item.key)}
-      />
+      <Navbar navLinks={navLinks} />
     </S.Container>
   );
 }
 
 Sidebar.propTypes = {
-  onNavClick: PropTypes.func.isRequired,
   links: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
